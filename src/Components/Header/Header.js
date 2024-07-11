2eed7c554da6f0b1b27b5f548124658c3716220d
mihail kiyamov <mihailkyamov@yandex.ru>
2024-07-11 12:59:06 +0500

    0.9

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,19 +1,20 @@
 import "./Header.css";
 import logo from "../../images/refregirator_icon.png";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="Лого" />
-      <a href="./" className="header__link">
+      <Link to="/" className="header__link">
         Организовать
-      </a>
-      <a href="./saved" className="header__link">
+      </Link>
+      <Link to="/saved" className="header__link">
         Сохраненые
-      </a>
-      <a href="./about" className="header__link">
+      </Link>
+      <Link to="/about" className="header__link">
         О проекте
-      </a>
+      </Link>
     </header>
   );
 }
