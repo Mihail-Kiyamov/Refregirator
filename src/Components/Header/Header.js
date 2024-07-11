753c3f62e3b8f8370409ff587f8e898beb677c14
mihail kiyamov <mihailkyamov@yandex.ru>
2024-07-11 12:41:42 +0500

    0.9

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,13 +5,13 @@ function Header() {
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="Лого" />
-      <a href="/" className="header__link">
+      <a href="./" className="header__link">
         Организовать
       </a>
-      <a href="/saved" className="header__link">
+      <a href="./saved" className="header__link">
         Сохраненые
       </a>
-      <a href="/about" className="header__link">
+      <a href="./about" className="header__link">
         О проекте
       </a>
     </header>
